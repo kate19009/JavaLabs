a79dabc2853d624a89d1e6b7651244b98f88ec42
kate19009 <44441442+kate19009@users.noreply.github.com>
2018-11-01 17:43:49 +0300

    Update Main.java

--- Lab1/src/Main.java
+++ Lab1/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main
         {
-            public static void main()
+            public static void main(String[] args)
             {
                     int size;
                     Scanner in = new Scanner(System.in);
