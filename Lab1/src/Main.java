0d20d9d234b5d0e70b4d029714544f2658ad622a
kate19009 <44441442+kate19009@users.noreply.github.com>
2018-11-01 16:00:57 +0300

    Add files via upload

---
+++ Lab1/src/Main.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class Main
+        {
+            public static void main()
+            {
+                    int size;
+                    Scanner in = new Scanner(System.in);
+                    System.out.println("Input matrix size");
+                    size = in.nextInt();
+                    int matrixL [][] = new int[size][size];
+                    for(int i=0;i<size;i++)
+                    {
+                        for (int j=0; j<size; j++)
+                        {
+                            if (i==j ||  i+j==size-1)
+                            {
+                                matrixL [i][j] = 1;
+                            }
+                            else matrixL [i][j] = 0;
+                        }
+                    }
+
+                    for(int i=0;i<size;i++)
+                    {
+                        for (int j=0; j<size; j++)
+                        {
+                           System.out.print(matrixL[size][size] + "\t");
+                        }
+                    }
+                    System.out.println();
+
+            }
+        }
